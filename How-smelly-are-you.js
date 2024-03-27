620cc49a32a4812f2a94a6b62bd95ce58b664cfd
Thezami <165180227+Thezami@users.noreply.github.com>
2024-03-28 07:43:34 +0800

    Add files via upload

--- How-smelly-are-you.js
+++ How-smelly-are-you.js
@@ -1,7 +1,4 @@
-document.querySelector('.check-button')
-    .addEventListener('click', () => {
-        smellChecker ()
-    });
+checkButton ()
 
 function smellChecker (){
     const smell = Math.random();
@@ -9,22 +6,67 @@ function smellChecker (){
 
     if (smell <= 1/4){
         smellResult = 'You damn fking smelly'
-    } else if (1/4 < smell <= 2/4){
+    } else if (smell > 1 / 4 && smell <= 2 / 4){
         smellResult = 'You are quite smelly'
-    } else if (2/4 < smell <= 3/4){
+    } else if (smell > 2 / 4 && smell <= 3 / 4){
         smellResult = 'You are ok smelling'
-    } else if (3/4 < smell <= 1){
+    } else {
         smellResult = 'You smell good!'
     }
 
     let smellResultHTML = `
     ${smellResult}
     <div>
-    <button onclick="smellChecker()">Check Again?!</button>
+    <button onclick="
+    checkButton();
+    loadResultCounter = 0
+    loadResult();
+    document.querySelector('.smell-result')
+        .innerHTML = '';
+    ">Check Again?!</button>
     </div>  
 `
     document.querySelector('.smell-result')
         .innerHTML = smellResultHTML;
 };
 
+function checkButton (){
+     document.querySelector('.check-button-div')
+        .innerHTML = '<button class="check-button">Check!</button>';
+    document.querySelector('.check-button').addEventListener('click',() => {loadResult()})
+};
 
+
+let loadResultCounter = 0
+
+function loadResult(){
+    let checkButtonHTML = 'loading';
+    document.querySelector('.check-button')
+    .innerHTML = checkButtonHTML;
+    let i = 0
+
+    loadingInterval = setInterval(() => {
+        checkButtonHTML +='.'
+        i++
+        console.log(checkButtonHTML);
+        console.log(i);
+        document.querySelector('.check-button')
+            .innerHTML = checkButtonHTML;
+
+        if (i === 3){
+            clearInterval(loadingInterval);
+            loadResult();
+            loadResultCounter ++;
+            console.log(loadResultCounter);
+        };
+
+        if (loadResultCounter === 3){
+            clearInterval(loadingInterval);
+            smellChecker();
+
+            document.querySelector('.check-button-div')
+                .innerHTML = '';            
+        }
+        }, 50);
+        
+};
